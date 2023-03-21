9d1c7f22e25c3578ca7aca38e73580a21408723b
Shane Standifur <shanestandifur@gmail.com>
2023-03-21 16:45:25 -0500

    stripe integration is now complete

---
+++ src/components/payments/SuccessMessage.js
@@ -0,0 +1,14 @@
+import "./SuccessMessage.css"
+
+export const SuccessMessage = () => {
+
+    return <>
+        <div id="profile_card" className="card" style={{ border: '0', textAlign: 'center' }}>
+            <img src="https://www.citypng.com/public/uploads/preview/marvel-comics-logo-hd-png-11662676291aldykcgywp.png" alt="Marvel Comics logo" style={{ width: '200px', height: '100px', margin: '0 auto' }} />
+            <h1>Order placed!</h1>
+            <p>Thank you for your purchase of Marvel comics books. Your order is on its way!</p>
+        </div>
+
+
+    </>
+}
--- src/components/payments/TestPayment.js
+++ src/components/payments/TestPayment.js
@@ -1,96 +1,96 @@
-import { getSpecificUserShoppingCart } from "../managers/CartManager"
-import { useState, useEffect } from "react"
-import { useNavigate } from "react-router-dom"
-import "./TestPayment.css"
+// import { getSpecificUserShoppingCart } from "../managers/CartManager"
+// import { useState, useEffect } from "react"
+// import { useNavigate } from "react-router-dom"
+// import "./TestPayment.css"
 
-const ProductDisplay = () => (
-    <section id="shopping_cart_section">
-        <div id="product">
-            <img
-                id="image"
-                src="https://cdn.marvel.com/u/prod/marvel/i/mg/f/10/598363848588e/detail.jpg"
-                alt="The cover of Stubborn Attachments"
-            />
-            <div id="description">
-                <h3 id="shopping_cart_text">Amazing Fantasy 15</h3>
-                <h5 id="shopping_cart_text">$10.00</h5>
-            </div>
-        </div>
-        <form action="http://localhost:4242/create-checkout-session" method="POST">
-            <button type="submit" class="btn btn-danger">
-                Checkout
-            </button>
-        </form>
-    </section>
-)
+// const ProductDisplay = () => (
+//     <section id="shopping_cart_section">
+//         <div id="product">
+//             <img
+//                 id="image"
+//                 src="https://cdn.marvel.com/u/prod/marvel/i/mg/f/10/598363848588e/detail.jpg"
+//                 alt="The cover of Stubborn Attachments"
+//             />
+//             <div id="description">
+//                 <h3 id="shopping_cart_text">Amazing Fantasy 15</h3>
+//                 <h5 id="shopping_cart_text">$10.00</h5>
+//             </div>
+//         </div>
+//         <form action="http://localhost:4242/create-checkout-session" method="POST">
+//             <button type="submit" class="btn btn-danger">
+//                 Checkout
+//             </button>
+//         </form>
+//     </section>
+// )
 
-const Message = ({ message }) => (
-    <section>
-        <p id="payment_text">{message}</p>
-    </section>
-)
+// const Message = ({ message }) => (
+//     <section>
+//         <p id="payment_text">{message}</p>
+//     </section>
+// )
 
-export const TestPayment = () => {
-    const [allCarts, setAllCarts] = useState([])
-    const [message, setMessage] = useState("")
-    const [showProductDisplay, setShowProductDisplay] = useState(true)
-    const navigate = useNavigate()
+// export const TestPayment = () => {
+//     const [allCarts, setAllCarts] = useState([])
+//     const [message, setMessage] = useState("")
+//     const [showProductDisplay, setShowProductDisplay] = useState(true)
+//     const navigate = useNavigate()
 
-    useEffect(() => {
-        getSpecificUserShoppingCart().then((data) => {
-            setAllCarts(data)
-        })
-    }, [])
+//     useEffect(() => {
+//         getSpecificUserShoppingCart().then((data) => {
+//             setAllCarts(data)
+//         })
+//     }, [])
 
-    useEffect(() => {
-        // Check to see if this is a redirect back from Checkout
-        const query = new URLSearchParams(window.location.search)
+//     useEffect(() => {
+//         // Check to see if this is a redirect back from Checkout
+//         const query = new URLSearchParams(window.location.search)
 
-        if (query.get("success")) {
-            const successMessage =
-                "Order placed! You will receive an email confirmation."
-            setMessage(successMessage)
-            setShowProductDisplay(false)
-        }
+//         if (query.get("success")) {
+//             const successMessage =
+//                 "Order placed! You will receive an email confirmation."
+//             setMessage(successMessage)
+//             setShowProductDisplay(false)
+//         }
 
-        if (query.get("canceled")) {
-            setMessage(
-                "Order canceled -- continue to shop around and checkout when you're ready."
-            )
-            setShowProductDisplay(false)
-        }
-    }, [])
+//         if (query.get("canceled")) {
+//             setMessage(
+//                 "Order canceled -- continue to shop around and checkout when you're ready."
+//             )
+//             setShowProductDisplay(false)
+//         }
+//     }, [])
 
-    return (
-        <>
-            {allCarts.map((cart) => {
-                return (
-                    <section id="shopping_cart_section" key={cart.id}>
-                        {cart.comics.map((comic) => (
-                            <div id="product" key={comic.id}>
-                                <img
-                                    id="image"
-                                    src={`${comic.comic_picture}.${comic.comic_extension}`}
-                                />
-                                <div id="description">
-                                    <h3 id="shopping_cart_text">{comic.comic_title}</h3>
-                                    <h5 id="shopping_cart_text">$10.00</h5>
-                                </div>
-                            </div>
-                        ))}
-                        <form
-                            action="http://localhost:4242/create-checkout-session"
-                            method="POST"
-                        >
-                            <button type="submit" class="btn btn-danger">
-                                Checkout
-                            </button>
-                        </form>
-                    </section>
-                )
-            })}
-            {message && <Message message={message} />}
-            {showProductDisplay && <ProductDisplay />}
-        </>
-    )
-}
+//     return (
+//         <>
+//             {allCarts.map((cart) => {
+//                 return (
+//                     <section id="shopping_cart_section" key={cart.id}>
+//                         {cart.comics.map((comic) => (
+//                             <div id="product" key={comic.id}>
+//                                 <img
+//                                     id="image"
+//                                     src={`${comic.comic_picture}.${comic.comic_extension}`}
+//                                 />
+//                                 <div id="description">
+//                                     <h3 id="shopping_cart_text">{comic.comic_title}</h3>
+//                                     <h5 id="shopping_cart_text">$10.00</h5>
+//                                 </div>
+//                             </div>
+//                         ))}
+//                         <form
+//                             action="http://localhost:4242/create-checkout-session"
+//                             method="POST"
+//                         >
+//                             <button type="submit" class="btn btn-danger">
+//                                 Checkout
+//                             </button>
+//                         </form>
+//                     </section>
+//                 )
+//             })}
+//             {message && <Message message={message} />}
+//             {showProductDisplay && <ProductDisplay />}
+//         </>
+//     )
+// }
